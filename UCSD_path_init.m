365cd4a65b07d67d3311e737c1b37a5a6f6d1d3e
j-mpy <jadenmcdonaldpeltieryu@gmail.com>
2023-08-14 23:53:47 -0700

    Create UCSD_path_init.m

---
+++ UCSD_path_init.m
@@ -0,0 +1,9 @@
+
+format compact, close all
+% IMPORTANT: modify the definition of NRbase in the startup.m code (see above) to indicate the
+% full path to the NR codebase on your computer.
+addpath(strcat(UCSD,'/MAE-40'),strcat(UCSD,'/MATH_20D'),...
+    strcat(UCSD,'/MAE_107'),strcat(UCSD,'/MAE_8'),...
+    UCSDbase)
+
+% end script UCSD_path_init
