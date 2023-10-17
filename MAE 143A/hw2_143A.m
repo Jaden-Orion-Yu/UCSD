1090a705b8fc41b1858609e28ca95f1f79ef6ea9
j-mpy <jadenmcdonaldpeltieryu@gmail.com>
2023-10-17 09:47:36 -0700

    HW2 finished

--- MAE 143A/hw2_143A.m
+++ MAE 143A/hw2_143A.m
@@ -1,1 +1,26 @@
 clear;clc;
+
+%p3 check
+syms s X1 X2 X3 x1 x2 x3;
+
+X1 = (s + 4)/((s*(s+2)*(s+3)));
+
+X2 = (11*(s+1))/(s^2 + 121);
+
+X3 = (1-exp(-8*s))*(2*s + 80)/((s+4)*(s+2));
+
+x1 = ilaplace(X1);
+x2 = ilaplace(X2);
+x3 = ilaplace(X3);
+
+%% p2 check
+syms X Y;
+
+e1 = (s*X) + (3*X) + (8*X) ==1;
+e2 = (s*Y) + (3*Y) - (3*X) ==2;
+
+sol = solve([e1,e2],[X,Y])
+
+x = ilaplace(sol.X)
+y = ilaplace(sol.Y)
+
